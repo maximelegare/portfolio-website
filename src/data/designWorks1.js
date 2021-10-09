f610df5bb4b925eb8fb9de84a997be00a39694ae
Maxime Legare <mleg1234@protonmail.com>
2021-10-09 11:38:19 -0400

    add photography icon & france Lavoie headshot

--- src/data/designWorks1.js
+++ src/data/designWorks1.js
@@ -1,18 +1,6 @@
-// import imgT1 from "../assets/image/jpg/agency/masonry/portfolio-mas-8.jpg";
-// import imgT2 from "../assets/image/jpg/agency/masonry/portfolio-mas-7.jpg";
-// import imgT3 from "../assets/image/jpg/agency/masonry/portfolio-mas-6.jpg";
-// import imgT4 from "../assets/image/jpg/agency/masonry/portfolio-mas-5.jpg";
-// import imgT5 from "../assets/image/jpg/agency/masonry/portfolio-mas-4.jpg";
-// import imgT6 from "../assets/image/jpg/agency/masonry/portfolio-mas-3.jpg";
-// import imgT7 from "../assets/image/jpg/agency/masonry/portfolio-mas-16.jpg";
-// import imgT8 from "../assets/image/jpg/agency/masonry/portfolio-mas-15.jpg";
-// import imgT9 from "../assets/image/jpg/agency/masonry/portfolio-mas-2.jpg";
-// import imgT10 from "../assets/image/jpg/agency/masonry/portfolio-mas-1.jpg";
-// import imgT11 from "../assets/image/jpg/agency/masonry/portfolio-mas-12.jpg";
-// import imgT12 from "../assets/image/jpg/agency/masonry/portfolio-mas-11.jpg";
-// import imgT13 from "../assets/image/jpg/agency/masonry/portfolio-mas-14.jpg";
-// import imgT14 from "../assets/image/jpg/agency/masonry/portfolio-mas-13.jpg";
 
+
+import imgT1 from "../assets/my-images/jpg/01_france-lavoie.jpg";
 import imgT3 from "../assets/my-images/jpg/03_fed-by-music.jpg";
 import imgT4 from "../assets/my-images/jpg/04_buisness-card-stylisme.jpg";
 import imgT5 from "../assets/my-images/jpg/05_bikanky-landing.jpg";
@@ -26,19 +14,20 @@ import imgT12 from "../assets/my-images/jpg/12_photoshoot-stylisme.jpg";
 import imgT13 from "../assets/my-images/jpg/13_photoshoot-stylisme.jpg";
 import imgT14 from "../assets/my-images/jpg/14_photoshoot-stylisme.jpg";
 
+import photography from '../assets/my-images/svg/technologies/camera3.svg'
 import illustrator from '../assets/my-images/svg/technologies/illustrator.svg'
 import firebase from '../assets/my-images/svg/technologies/firebase.svg'
-import nodejs from '../assets/my-images/svg/technologies/nodejs.svg'
+// import nodejs from '../assets/my-images/svg/technologies/nodejs.svg'
 import photoshop from '../assets/my-images/svg/technologies/photoshop.svg'
 import react from '../assets/my-images/svg/technologies/react.svg'
 import redux from '../assets/my-images/svg/technologies/redux.svg'
 import sass from '../assets/my-images/svg/technologies/sass.svg'
 import stripe from '../assets/my-images/svg/technologies/stripe.svg'
 import vuejs from '../assets/my-images/svg/technologies/vuejs.svg'
-import mongodb from '../assets/my-images/svg/technologies/mongodb.svg'
+// import mongodb from '../assets/my-images/svg/technologies/mongodb.svg'
 import materialui from '../assets/my-images/svg/technologies/materialui.svg'
 import styledcomponent from '../assets/my-images/svg/technologies/styledcomponent.svg'
-import express from '../assets/my-images/svg/technologies/express.svg'
+// import express from '../assets/my-images/svg/technologies/express.svg'
 
 
 
@@ -50,7 +39,7 @@ export const designWorks1 = [
     categories: [`graphisme`],
     thumbnail: imgT4,
     link: "/portfolio-details-2",
-    technologies:[illustrator, photoshop]
+    technologies:[illustrator, photoshop, photography]
   },
   {
     brand: `Création de vêtements`,
@@ -59,7 +48,7 @@ export const designWorks1 = [
     categories: [`developpement web`, `photographie`, `ui-design`],
     thumbnail: imgT5,
     link: "/portfolio-details-2",
-    technologies:[react, redux, firebase, stripe, materialui, styledcomponent, photoshop, illustrator,]
+    technologies:[react, redux, firebase, stripe, materialui, styledcomponent, photoshop, illustrator, photography]
   },
   {
     brand: `Stylisme`,
@@ -68,7 +57,16 @@ export const designWorks1 = [
     categories: [`ui-design`, `photographie`],
     thumbnail: imgT6,
     link: "/portfolio-details-2",
-    technologies:[illustrator]
+    technologies:[illustrator, photoshop, photography]
+  },
+  {
+    brand: `Stylisme`,
+    slug: `stylisme`,
+    title: `Adaptable but Identifiable Product for Kandinsky.`,
+    categories: [ `photographie`],
+    thumbnail: imgT1,
+    link: "/portfolio-details-2",
+    technologies:[photoshop, photography]
   },
   {
     brand: `Timers & Todos`,
@@ -77,7 +75,7 @@ export const designWorks1 = [
     categories: [`developpement web`,`ui-design`],
     thumbnail: imgT7,
     link: "/portfolio-details-2",
-    technologies:[vuejs, mongodb, nodejs, express]
+    technologies:[vuejs, firebase]
   },
   {
     brand: `Photographie d'horreur`,
@@ -86,7 +84,7 @@ export const designWorks1 = [
     categories: [`photographie`, `graphisme`],
     thumbnail: imgT3,
     link: "/portfolio-details-2",
-    technologies:[photoshop]
+    technologies:[photoshop, photography]
   },
   {
     brand: `Photographie d'horreur`,
@@ -95,7 +93,7 @@ export const designWorks1 = [
     categories: [`photographie`],
     thumbnail: imgT8,
     link: "/portfolio-details-2",
-    technologies:[photoshop]
+    technologies:[photoshop, photography]
   },
   {
     brand: `Trouver un mentor`,
@@ -131,7 +129,7 @@ export const designWorks1 = [
     categories: [`photogrphie`],
     thumbnail: imgT12,
     link: "/portfolio-details-2",
-    technologies:[photoshop]
+    technologies:[photoshop, photography]
   },
   {
     brand: `Stylisme`,
@@ -140,7 +138,7 @@ export const designWorks1 = [
     categories: [`photographie`],
     thumbnail: imgT13,
     link: "/portfolio-details-2",
-    technologies:[photoshop]
+    technologies:[photoshop, photography]
   },
   {
     brand: `Stylisme`,
@@ -149,6 +147,6 @@ export const designWorks1 = [
     categories: [`photographie`],
     thumbnail: imgT14,
     link: "/portfolio-details-2",
-    technologies:[photoshop]
+    technologies:[photoshop, photography]
   }
 ];
